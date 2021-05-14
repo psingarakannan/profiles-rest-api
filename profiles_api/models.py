1727b0e2785672e8ade725ddcb4af6b65905863a
ps3 <ps3@vmware.com>
2021-05-14 10:53:15 +0530

    Adding Hello World Get request

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
+
+
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
